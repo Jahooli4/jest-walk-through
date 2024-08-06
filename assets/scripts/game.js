d250558ff34ccecbec8e5cc34a4a656470731aa0
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2024-08-06 12:02:23 +0000

    add game tests inc choices button array

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,10 @@
 
 
 let game = {
-    score = 0,
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
 }
 
-module exports = { game }; 
+module.exports = { game }; 
